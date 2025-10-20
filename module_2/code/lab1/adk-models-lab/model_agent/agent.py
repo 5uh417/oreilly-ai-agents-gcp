da20f76610b76ef1f80660455a0f9521b3e4261b
Suhail Mirza <smirza@protonmail.com>
2025-10-20 21:14:23 +0530

    New codes

--- module_2/code/lab1/adk-models-lab/model_agent/agent.py
+++ module_2/code/lab1/adk-models-lab/model_agent/agent.py
@@ -1,12 +1,13 @@
-from dotenv import load_dotenv
 from google.adk.agents import LlmAgent
+from google.adk.models.lite_llm import LiteLlm
+from dotenv import load_dotenv
 
 # Load environment variables
 load_dotenv()
 
 root_agent = LlmAgent(
     name="weather_time_agent",
-    model="gemini-2.0-flash",
+    model=LiteLlm(model="anthropic/claude-3-haiku-20240307"),
     description=(
         "Agent to answer questions about the time and weather in a city."
     ),
---
+++ module_2/code/lab3/adk-openapi-lab/petstore_agent/__init__.py
@@ -0,0 +1,2 @@
+# petstore_agent/__init__.py
+from . import agent
